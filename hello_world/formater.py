48006180d2ba5591a0855026a5bc796ffd0c6016
kodi3453 <kolodziejkodi72@gmail.com>
2026-07-06 15:01:54 +0200

    Makefile + lint + test working

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -1,5 +1,3 @@
-import json
-
 PLAIN = "plain"
 PLAIN_UP = "plain_uppercase"
 PLAIN_LO = "plain_lowercase"
@@ -9,23 +7,18 @@ SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON]
 
 
 def get_formatted(msg, imie, format):
-    result = ""
     if format == PLAIN:
-        result = plain_text(msg, imie)
+        return plain_text(msg, imie)
     elif format == PLAIN_UP:
-        result = plain_text_upper_case(msg, imie)
+        return plain_text_upper_case(msg, imie)
     elif format == PLAIN_LO:
-        result = plain_text_lower_case(msg, imie)
+        return plain_text_lower_case(msg, imie)
     elif format == JSON:
-        result = format_to_json(msg, imie)
-    return result
+        return format_to_json(msg, imie)
 
 
 def format_to_json(msg, imie):
-    return json.dumps({
-        "imie": imie,
-        "msg": msg
-    })
+    return '{ "imie":"' + imie + '", "mgs":' + msg + '"}'
 
 
 def plain_text(msg, imie):
--- hello_world/views.py
+++ hello_world/views.py
@@ -1,18 +1,16 @@
 from hello_world import app
-from hello_world.formater import get_formatted
-from hello_world.formater import SUPPORTED, PLAIN
+from hello_world.formater import get_formatted, SUPPORTED
 from flask import request
 
-moje_imie = "Konrad"
+moje_imie = "Konrad"# noqa
 msg = "Hello World!"
+
 
 @app.route('/')
 def index():
-    output = request.args.get('output')
-    if not output:
-        output = PLAIN
-    return get_formatted(msg, moje_imie,
-                         output.lower())
+    output = request.args.get('output', 'plain')
+    return get_formatted(msg, moje_imie, output.lower())
+
 
 @app.route('/outputs')
 def supported_output():
